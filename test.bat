a48226c5452780346224aa47da1ed62dc54d445a
Edison Lin <edison_lin@quantatw.com>
2023-10-27 09:37:03 +0800

    [2023/10/27] - 1

--- test.bat
+++ test.bat
@@ -1,5 +1,5 @@
 @REM powershell start-process c:\Users\edison\Downloads\NewRepo-master\TestManager\TestManager\bin\Debug\TestManager.exe -verb RunAs
 powershell set-executionpolicy Bypass -scope CurrentUser
 powershell set-executionpolicy Bypass -scope process
-powershell start-process c:\TestManager\TestManager.exe -verb RunAs
+powershell start-process c:\TestManager\TestManager\TestManager\bin\Debug\TestManager.exe -verb RunAs
 exit
